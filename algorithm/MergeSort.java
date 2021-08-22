07f9ae2ba0767b8d7a37d19e9e1f7b5342560758
vicetone <260294702@qq.com>
2021-08-23 00:11:40 +0800

    2021-08-23

--- algorithm/MergeSort.java
+++ algorithm/MergeSort.java
@@ -19,23 +19,30 @@ public class MergeSort {
     }
 
     public static void sort(int[] arr) {
-        int[] temp = new int[arr.length];//在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+        //在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+        int[] temp = new int[arr.length];
         sort(arr, 0, arr.length - 1, temp);
     }
 
     private static void sort(int[] arr, int left, int right, int[] temp) {
         if (left < right) {
             int mid = (left + right) / 2;
-            sort(arr, left, mid, temp);//左边归并排序，使得左子序列有序
-            sort(arr, mid + 1, right, temp);//右边归并排序，使得右子序列有序
-            merge(arr, left, mid, right, temp);//将两个有序子数组合并操作
+            //左边归并排序，使得左子序列有序
+            sort(arr, left, mid, temp);
+            //右边归并排序，使得右子序列有序
+            sort(arr, mid + 1, right, temp);
+            //将两个有序子数组合并操作
+            merge(arr, left, mid, right, temp);
         }
     }
 
     private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-        int i = left;//左序列指针
-        int j = mid + 1;//右序列指针
-        int t = 0;//临时数组指针
+        //左序列指针
+        int i = left;
+        //右序列指针
+        int j = mid + 1;
+        //临时数组指针
+        int t = 0;
         while (i <= mid && j <= right) {
             if (arr[i] <= arr[j]) {
                 temp[t++] = arr[i++];
@@ -45,10 +52,12 @@ public class MergeSort {
                 temp[t++] = arr[j++];
             }
         }
-        while (i <= mid) {//将左边剩余元素填充进temp中
+        //将左边剩余元素填充进temp中
+        while (i <= mid) {
             temp[t++] = arr[i++];
         }
-        while (j <= right) {//将右序列剩余元素填充进temp中
+        //将右序列剩余元素填充进temp中
+        while (j <= right) {
             temp[t++] = arr[j++];
         }
         t = 0;
